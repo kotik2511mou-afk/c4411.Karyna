a33133a6f1db0fe2d327fdbfb4ce87afc8c4790c
Simson <kotik25.11mou@gmail.com>
2025-09-20 10:12:09 +0300

    intro & sys

---
+++ para  5_4.py
@@ -0,0 +1,6 @@
+data = "Vasya Pupkin"
+def f_func():
+    pass
+
+print(callable(data))
+print(callable(f_func))
---
+++ para 5_7.py
@@ -0,0 +1,7 @@
+import  sys
+import requests
+import  random
+
+for modudle_name, module_path in sys.modules.items():
+    print(modudle_name,module_path)
+
